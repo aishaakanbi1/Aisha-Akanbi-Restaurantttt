cab47e6b56be90495ad306f5f63cfc1440f7608f
aisha akanbi <adesuwaakanbi60@gmail.com>
2023-09-14 02:19:47 +0100

    finalized

--- index.js
+++ index.js
@@ -1,32 +1,81 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
-window.onscroll = () =>{
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
-document.querySelector('#search-icon').onclick = () =>{
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector(`header .navbar a[href*=${id}]`).classList.add('active');
+        }
+    });
+};
+
+document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
-}
+};
 
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active')
-}
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+};
 
-var swiper = new Swiper(".home-slider", {
+var homeSwiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-   loop: true,
-  });
+    loop: true,
+});
+
+var reviewSwiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+setTimeout(loader, 3000); // Add the 'fade-out' class after a delay of 3 seconds
+
